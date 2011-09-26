92c5f899256de3ef7f7724c140e314510b045617
RyanMurphy <RyanMurphy@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-09-26 16:26:54 +0000

    Tagging 1.1
    git-svn-id: https://plugins.svn.wordpress.org/use-domain-shortlink/tags/1.1@443745 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- use-domain-shortlink.php
+++ use-domain-shortlink.php
@@ -1,9 +1,9 @@
 <?php 
 /*
 Plugin Name: Use Domain Shortlink (for Twitter Tools)
-Plugin URI: http://2skewed.net 
+Plugin URI: http://2skewed.net/plugins/use-domain-shortlink/
 Description: Use wp_get_shortlink() to send the site's internal short URL (i.e. http://domain.com/?p=1234) to twitter. Requires Twitter Tools.
-Version: 1
+Version: 1.1
 Author: Ryan Murphy
 Author URI: http://2skewed.net
 */
@@ -19,46 +19,36 @@ THIS SOFTWARE IS PROVIDED `AS IS` AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDI
 
 function rpm_uds_use_shortlink() {
 	//get the post, find the shortlink, and return it
-	//since this only gets fired if Twitter Tools is active, we don't need to check for it.
-	//Do I need to check for wp_get_shortlink again though?...
 	global $post;
 	$shortlink = wp_get_shortlink($post->ID, 'post');
 	return $shortlink;
 }
-function rpm_uds_option_field() { ?>
-	<h2>Use Domain Shortlink</h2>
-	<?php 
-	//check to make sure that the function we rely on is availible. If it is, show the setting field so the user can turn it on.
-	if (function_exists( 'wp_get_shortlink' ) ) { ?>
-		<p>Check this box if you wish to use the site's internal shortlink (e.g. <?php echo home_url(); ?>/?p=1234, where 1234 is the post's ID).</p>
-		<form method='post' action='options.php'>
-			<?php settings_fields( 'rpm_uds_setting' ); 
-			//we only have one option, so let's not bother with a full blown table ?>
-			<p>Enable Internal Shortlink <input type='checkbox' name='rpm_uds_active' <?php if( get_option( 'rpm_uds_active' ) == 'on') echo "checked='checked' "; ?> /></p>
-			<p class="submit">
-				<input type="submit" class="button-secondary" value="Save Changes" />
-			</p>
-		</form>
-	<?php }
-	//if the function we need doesn't exist, tell the user that they need to update WordPress
-	else {
-	global $wp_version; ?>
-	<p>The function this plugin relies on does not exist. This could mean that you are using an outdated version of WordPress, or have an incomplete installation.<br />
-	This plugin requires at least WordPress 3.0 in order to function. You are currently using WordPress <?php echo $wp_version; ?>; please consider <a href='<?php echo get_admin_url();?>update-core.php' title='Update WordPress'>updating to the latest version</a>. If you are using WordPress 3.0 or greater, you should <a href='<?php echo get_admin_url();?>update-core.php'>reinstall WordPress</a></p>
-	<?php }
 
+//Add admin notices if WP<3.0 or Twitter Tools not installed/activated.
+function rpm_uds_setup_notice() {
+	if( !function_exists( 'wp_get_shortlink' ) ) {
+		echo '<div class="updated fade"><p><strong>Notice:</strong> Use Domain Shortlink requires at least WordPress 3.0. Please <a href="'. get_admin_url().'"update-core.php">update or reinstall</a> if you see this notice.</p></div>';
+	}
+	if( !is_plugin_active( 'twitter-tools/twitter-tools.php' ) ) {
+		echo '<div class="updated"><p><strong>Notice:</strong> It appears that <a href="http://wordpress.org/extend/plugins/twitter-tools/" title="WordPress &#8250; Twitter Tools &laquo; WordPress Plugins">Twitter Tools</a> is not installed. <strong>Use Domain Shortlink</strong> requires Twitter Tools to be installed and activated to function.</p></div>';
+	}
 }
+
 if ( is_admin() ) {
-	add_action( 'aktt_options_form', 'rpm_uds_option_field' );
-	add_action( 'admin_init', 'rpm_register_uds_setting' );
+	add_action( 'admin_notices', 'rpm_uds_setup_notice' );
 }
 
-//only add the filter if the plugin is activated and turned on (via the setting)
-if ( get_option( 'rpm_uds_active' ) == 'on' ) {
+//only add the filter if wp_get_shortlink() exists.
+if ( function_exists( 'wp_get_shortlink' ) ) {
 	add_filter( 'tweet_blog_post_url', 'rpm_uds_use_shortlink' );
 }
 
-function rpm_register_uds_setting() {
-	register_setting( 'rpm_uds_setting', 'rpm_uds_active' );
+//Decisions, not options: Get rid of the single option that plugin had. Users will now be told if they need to update via an admin notice when the plugin is activated. Also tells them if Twitter Tools isn't installed and activated.
+register_activation_hook( __FILE__, 'rpm_uds_del_option' );
+
+function rpm_uds_del_option() {
+	unregister_setting( 'rpm_uds_setting', 'rpm_uds_active' );
+	delete_option( 'rpm_uds_active' );
 }
+
 ?>
